0a820907984c40a71a93474449e07711916fa716
darwgom7 <darwgom7@gmail.com>
2023-09-17 21:51:18 -0500

    initial commit

---
+++ src/main/java/org/darwgom/FutureknowledgeschoolApiApplication.java
@@ -0,0 +1,13 @@
+package org.darwgom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureknowledgeschoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureknowledgeschoolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darwgom/FutureknowledgeschoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwgom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureknowledgeschoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
